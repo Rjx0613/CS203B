626372fc5bcc7201649f18d721239912e1ebfc52
Rjx0613 <12110613@mail.sustech.edu.cn>
2022-11-29 20:49:27 +0800

    1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Main {
+    public static void main(String[] args) {
+        find find=new find();
+        int row= StdIn.readInt();
+        int col=StdIn.readInt();
+        int[][] inputBoard=new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                inputBoard[i][j]= StdIn.readInt();
+            }
+        }
+        int[][] result= find.finalState(inputBoard);
+
+        while(find.open.size()!=0){
+            int i=0;
+        }
+    }
+}
---
+++ src/Point.java
@@ -0,0 +1,66 @@
+public class Point {
+    private int col, row;
+    private int[][] board;
+    private int F, G;
+
+    Point father;
+
+    public Point(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Point(int row, int col, int[][] board) {
+        this.row = row;
+        this.col = col;
+        this.board = board;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(int[][] board) {
+        this.board = board;
+    }
+
+    public int getF() {
+        return F;
+    }
+
+    public void setF(int f) {
+        F = f;
+    }
+
+    public int getG() {
+        return G;
+    }
+
+    public void setG(int g) {
+        G = g;
+    }
+
+    public Point getFather() {
+        return father;
+    }
+
+    public void setFather(Point father) {
+        this.father = father;
+    }
+}
